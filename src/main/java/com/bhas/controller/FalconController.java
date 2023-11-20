03cbb538e5d6b659a0c2f8618b4961ebf2e240cd
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-20 21:06:45 +0530

    <finalName>Falcon_App</finalName>

---
+++ src/main/java/com/bhas/controller/FalconController.java
@@ -0,0 +1,15 @@
+package com.bhas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FalconController
+{
+    @GetMapping
+    public String greetMsg()
+    {
+        System.out.println("\n*************");
+        return "Welcome to the Falcon Applications!,";
+    }
+}
