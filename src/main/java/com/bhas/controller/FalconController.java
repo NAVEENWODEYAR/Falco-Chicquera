372fdb88401204a9d8508c97ffa06b08def78adb
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-21 23:35:14 +0530

    logOut

--- src/main/java/com/bhas/controller/FalconController.java
+++ src/main/java/com/bhas/controller/FalconController.java
@@ -1,6 +1,8 @@
 package com.bhas.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,13 @@ public class FalconController
     {
         System.out.println("\n*************\n");
         return "Welcome to the Falcon Applications!,";
+    }
+
+    @GetMapping("/getName/{name}")
+    @ResponseStatus(code = HttpStatus.TOO_MANY_REQUESTS)
+    public String getName(String name)
+    {
+        System.out.println("\n*************\n");
+        return "Hello "+name;
     }
 }
