36940c18847691f9751d299e04aff61de5438186
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-22 22:50:54 +0530

    Entity class,

--- src/main/java/com/bhas/modal/Employee.java
+++ src/main/java/com/bhas/modal/Employee.java
@@ -1,6 +1,20 @@
 package com.bhas.modal;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+
 public class Employee
 {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empId;
+    private String empName;
+    private String empEmail;
+    private String empPhone;
+    private String empCategory;
+    private List<Address> address;
 }
