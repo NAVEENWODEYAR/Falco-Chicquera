7654e6927e5076b793e6adb3709e8c6c46501372
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-22 22:56:03 +0530

    <groupId>org.projectlombok</groupId>

--- src/main/java/com/bhas/modal/Address.java
+++ src/main/java/com/bhas/modal/Address.java
@@ -1,6 +1,15 @@
 package com.bhas.modal;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address
 {
-
+    private int addId;
+    private String addType;
+    private String addValue;
 }
