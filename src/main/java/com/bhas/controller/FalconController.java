9666ce06d41561af9b2131dfd8e44762cb0de61b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-22 22:46:58 +0530

    @RequestMapping("/falcon")

--- src/main/java/com/bhas/controller/FalconController.java
+++ src/main/java/com/bhas/controller/FalconController.java
@@ -2,10 +2,12 @@ package com.bhas.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/falcon")
 public class FalconController
 {
     @GetMapping
