1405ab410957510612f5d13da22bc4a1249391d6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-23 21:39:25 +0530

    CRUD operations

--- src/main/java/com/bhas/service/EmployeeService.java
+++ src/main/java/com/bhas/service/EmployeeService.java
@@ -1,9 +1,47 @@
 package com.bhas.service;
 
+import com.bhas.modal.Employee;
+import com.bhas.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService
 {
+    // DI,
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
+    // a. INSERT INTO TABLE VALUES();
+    public Employee insertEmployee(Employee emp)
+    {
+        return employeeRepo.save(emp);
+    }
+
+    // a.1 List<Employee>,
+    public List<Employee> insertEmployees(List<Employee> emp)
+    {
+        List<Employee> employees = employeeRepo.saveAll(emp);
+        return employees;
+    }
+
+    // b. SELECT * FROM EMPLOYEE;
+    public List<Employee> getEmployees()
+    {
+        return employeeRepo.findAll();
+    }
+
+    // c. SELECT * FROM EMPLOYEE WHERE empId="";,
+    public Employee getEmployeeById(int empId)
+    {
+        return employeeRepo.findById(empId).get();
+    }
+
+    // d. DELETE FROM table_name WHERE condition;
+    public String deleteEmpById(int empId)
+    {
+        employeeRepo.deleteById(empId);
+    }
 }
