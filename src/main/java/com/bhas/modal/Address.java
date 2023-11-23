6b8d1a387d3116c61e731181026ad7d800e24531
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-23 20:17:33 +0530

    @GeneratedValue(strategy = GenerationType.UUID)

--- src/main/java/com/bhas/modal/Address.java
+++ src/main/java/com/bhas/modal/Address.java
@@ -1,5 +1,9 @@
 package com.bhas.modal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +11,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Address
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private int addId;
     private String addType;
     private String addValue;
